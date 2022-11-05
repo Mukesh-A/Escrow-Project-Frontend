2622ae49409ac0ae89dd712eb4a57f599ae650f6
Mukesh-a <mukeshabhi08@gmail.com>
2022-11-05 14:49:19 +0530

    Day1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
+import { ethers } from "ethers";
+import detectEthereumProvider from "@metamask/detect-provider";
 
 function App() {
+  const [provider, setProvider] = useState(null);
+
+  useEffect(() => {
+    async function initate() {
+      const _provider = await detectEthereumProvider();
+      const provider = new ethers.providers.Web3Provider(_provider);
+
+      if (provider) {
+        setProvider(provider);
+      } else {
+        alert("install metamask");
+      }
+    }
+    initate();
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Escrow Contract app</h1>
+      <span>Active contract address {}</span>
     </div>
   );
 }
